05ddd1f3ab112de3d49d7148834a907d4ebf58df
Verthon <krzysiekksordyl@gmail.com>
2017-12-14 23:33:13 +0100

    vanilla js approach...

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Twitch.tv-API-App
+- doesnt work
+- https://stackoverflow.com/questions/47822319/accessing-to-any-element-of-array-return-undefined-jquery
+- https://stackoverflow.com/questions/14220321/how-do-i-return-the-response-from-an-asynchronous-call
 - using Twitch API
 - using jQuery
--- app.js
+++ app.js
@@ -1,35 +1,26 @@
-$(document).ready(function(){
-  const $users = ['freecodecamp', 'gumdropstsm', 'bregvids', 'studenalbatroz', 'warcraft'];
+const users = ['freecodecamp', 'gumdropstsm', 'bregvids', 'studenalbatroz', 'warcraft'];
   function getData(name){
-    let $usersData = [];
-    //getJSON is async. You can’t return anything...
-    //@ https://forum.jquery.com/topic/return-a-value-from-a-function-using-getjson
-    $.getJSON(`https://wind-bow.glitch.me/twitch-api/users/${name}`, function(data) {
-      $usersData[0] = data.display_name;
-      .done
-    });
-    $.getJSON(`https://wind-bow.glitch.me/twitch-api/streams/${name}`, function(data) {
-      $usersData[1] = data.stream;
-      //$usersData[2] = data.stream.channel.status;
-    });
-    $.getJSON(`https://wind-bow.glitch.me/twitch-api/channels/${name}`, function(data) {
-      $usersData[3] = data.logo;
-    });
-    return renderData($usersData);
+    let usersData = [];
+    let req1 = new XMLHttpRequest;
+    req1.open('GET',`https://wind-bow.glitch.me/twitch-api/streams/${name}`);
+      usersData[0] = data.display_name;
+
+    //
+      //usersData[2] = data.stream.channel.status;
+   //https://wind-bow.glitch.me/twitch-api/channels/${name}
+      //usersData[3] = data.logo;
+    return renderData(usersData);
   }
   function renderData(usersData){
-    /*
-    const $template = `      <li class="list-item">
+    const template = `      <li class="list-item">
     <img class="stream-logo" src="${usersData[3]}" alt="streamer-logo">
     <a class="stream-name" href="">${usersData[0]}</a>
     <p class="stream-title"></p>
   </li>`;
-  */
     console.log(usersData[0]);
-    $(".app-container #streamer-list").append('<li>'+usersData[0]+ '</li>');
+    //$(".app-container #streamer-list").append('<li>'+usersData[0]+ '</li>');
     return;
   }
-  for(let i = 0; i < $users.length; i++){
-    getData($users[i]);
+  for(let i = 0; i < users.length; i++){
+    getData(users[i]);
   }
-});
