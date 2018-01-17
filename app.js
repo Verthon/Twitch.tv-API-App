a4bb862e608f33441af2aec40f9e58d781623785
Verthon <krzysiekksordyl@gmail.com>
2018-01-17 21:33:55 +0100

    Major change, using javascript promise now!

--- app.js
+++ app.js
@@ -1,11 +1,70 @@
+//Users array only 5 chosen
 const users = ['freecodecamp', 'gumdropstsm', 'bregvids', 'studenalbatroz', 'warcraft'];
-  function getData(name){
-    let usersData = [];
-    let req1 = new XMLHttpRequest;
-    req1.open('GET',`https://wind-bow.glitch.me/twitch-api/streams/${name}`);
-      usersData[0] = data.display_name;
+  const get = (url) => {
+    return new Promise((resolve, reject) =>{
+      const xhttp = new XMLHttpRequest();
+      let data = null;
+      xhttp.open("GET", url, true);
+      xhttp.onload = () => {
+        if(xhttp.status === 200){
+          resolve(data = JSON.parse(xhttp.response));
+        }else{
+          reject(xhttp.statusText);
+        }
+      };
+      xhttp.onerror = () => {
+        reject(xhttp.statusText);
+      };
+      xhttp.send();
+    });
+  }
 
-    //
+  let getData = (name) => {
+    let streamData = {};
+    let addStreamer = (data) =>{
+      const template = `
+      <li class="list-item">
+        <img class="stream-logo" src="${streamData.logo}" alt="streamer-logo">
+        <a class="stream-name" href="${streamData.link}">${streamData.name}</a>
+        <p class="stream-title">${streamData.name}</p>
+      </li>
+      `;
+      const list = document.getElementById('streamer-list');
+      list.innerHTML += template;
+      console.log(streamData);
+      
+    }
+    let promise = get(`https://wind-bow.glitch.me/twitch-api/users/${name}`);
+    promise.then((data) => {
+      streamData.name = data.display_name;
+      streamData.logo = data.logo;
+      //console.log(streamData.name);
+      return get(`https://wind-bow.glitch.me/twitch-api/channels/${name}`);
+    }).then((data) => {
+      streamData.status = data.status;
+      streamData.link = data.url;
+      //console.log(data);
+      return get(`https://wind-bow.glitch.me/twitch-api/streams/${name}`);
+    }).then((data) => {
+      //console.log(data);
+    }).then((data) => {
+      return addStreamer(data);
+    }).catch((error) => {
+      console.log(error);
+    });
+  };
+  
+  /*
+  function getData(name){
+    let usersData = {};
+    let req1 = new XMLHttpRequest;
+    req1.open('GET',`https://wind-bow.glitch.me/twitch-api/users/${name}`);
+    req1.onload = () => {
+      let data = JSON.parse(this.responseText);
+      usersData.name = data.display_name;
+    }
+      //usersData[0] = data.display_name;
+    req1.send();
       //usersData[2] = data.stream.channel.status;
    //https://wind-bow.glitch.me/twitch-api/channels/${name}
       //usersData[3] = data.logo;
@@ -14,13 +73,11 @@ const users = ['freecodecamp', 'gumdropstsm', 'bregvids', 'studenalbatroz', 'war
   function renderData(usersData){
     const template = `      <li class="list-item">
     <img class="stream-logo" src="${usersData[3]}" alt="streamer-logo">
-    <a class="stream-name" href="">${usersData[0]}</a>
+    <a class="stream-name" href="">${usersData.name}</a>
     <p class="stream-title"></p>
   </li>`;
-    console.log(usersData[0]);
-    //$(".app-container #streamer-list").append('<li>'+usersData[0]+ '</li>');
+    console.log(usersData.name);  
     return;
   }
-  for(let i = 0; i < users.length; i++){
-    getData(users[i]);
-  }
+  */
+  users.forEach((user) => getData(user));
